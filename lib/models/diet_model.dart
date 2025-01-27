2038899395231f9c4706ef55cf962c4a5bc112a5
Sandra Simon <sandrasimon2301@gmail.com>
2025-01-27 15:45:23 +0530

    Project completed

---
+++ lib/models/diet_model.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class DietModel {
+  String name;
+  String iconPath;
+  String level;
+  String duration;
+  String calorie;
+  bool viewIsSelected;
+  Color boxColor;
+
+  DietModel(
+      {required this.name,
+      required this.iconPath,
+      required this.level,
+      required this.duration,
+      required this.calorie,
+      required this.viewIsSelected,
+      required this.boxColor});
+
+  static List<DietModel> getDiets() {
+    List<DietModel> diets = [];
+
+    diets.add(DietModel(
+        name: 'Honey Pancakes',
+        iconPath: 'assets/icons/honey-pancakes.svg',
+        level: 'Easy',
+        duration: '30mins',
+        calorie: '180kCal',
+        viewIsSelected: true,
+        boxColor: Color(0xff92A3FD)));
+
+    diets.add(DietModel(
+        name: 'Canai Bread',
+        iconPath: 'assets/icons/canai-bread.svg',
+        level: 'Easy',
+        duration: '20mins',
+        calorie: '230kCal',
+        viewIsSelected: true,
+        boxColor: Color(0xffC58BF2)));
+
+    return diets;
+  }
+}
